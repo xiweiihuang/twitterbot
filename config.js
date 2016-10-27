1374b6949461a40b795de1c40abfe2d3ea183c8d
xiweiihuang <eva.xw.h@gmail.com>
2016-10-26 22:19:29 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+// store twit config--API keys here
+ consumer_key:         'FrmHXHcGtlZemjjCxtuHILSnf',
+ consumer_secret:      '29RYoDvezpuhpvDWU0IFkaWsrxx5YeYNLLBnXj5INXLmOvVMmA',
+ access_token:         '741371180788133888-zNXsrijm9wzgWEDlfM3LXwwMY4B4xKv',
+ access_token_secret:  'OXvpb3CQpp7yMk1q4j8teDxArhQHIGhoZg50omq32k6jO',
+}
